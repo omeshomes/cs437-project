0afa25e5cfd73a7733410f46125951afc0fdc601
evanblasy <evanblasy@gmail.com>
2019-11-17 15:18:42 -0500

    Days display properly per month

--- frontend/src/WeatherHeader.js
+++ frontend/src/WeatherHeader.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useState} from 'react';
 import Navbar from 'react-bootstrap/Navbar';
 import Form from 'react-bootstrap/Form';
 import InputGroup from 'react-bootstrap/InputGroup';
@@ -6,30 +6,76 @@ import FormControl from 'react-bootstrap/FormControl';
 //import './App.css';
 
 function WeatherHeader() {
+    const [year,setYear] = useState(null);
+    const [month,setMonth] = useState(null);
+    const [day,setDay] = useState(null);
+
+    const dayOptions = [<option selected>Day...</option>];
+
+    for( let i = 0; i < 28; i++) {
+        dayOptions.push(<option key={i+1} value={i+1}>{i + 1}</option>);
+    }
+
     return (
         <Navbar className='navbar'>
             <Navbar.Brand> Weather Wars </Navbar.Brand>
             <Form inline>
                 <InputGroup>
                     <FormControl
-                        placeholder="Year"
-                        aria-label="Year"
-                        aria-describedby="basic-addon1"
-                    />
+                        as="select"
+                        onChange={(event) => setYear(event.target.value)}
+                    >
+                        <option selected>Year...</option>
+                        <option value='2010'>2010</option>
+                        <option value='2011'>2011</option>
+                        <option value='2012'>2012</option>
+                        <option value='2013'>2013</option>
+                        <option value='2014'>2014</option>
+                        <option value='2015'>2015</option>
+                        <option value='2016'>2016</option>
+                        <option value='2017'>2017</option>
+                        <option value='2018'>2018</option>
+                        <option value='2019'>2019</option>
+                    </FormControl>
                 </InputGroup>
                 <InputGroup>
-                    <FormControl
-                        placeholder="Month"
-                        aria-label="Month"
-                        aria-describedby="basic-addon1"
-                    />
+                <FormControl
+                        as="select"
+                        onChange={(event) => setMonth(event.target.value)}
+                    >
+                        <option selected>Month...</option>
+                        <option value='January'>January</option>
+                        <option value='February'>February</option>
+                        <option value='March'>March</option>
+                        <option value='April'>April</option>
+                        <option value='May'>May</option>
+                        <option value='June'>June</option>
+                        <option value='July'>July</option>
+                        <option value='August'>August</option>
+                        <option value='September'>September</option>
+                        <option value='October'>October</option>
+                        <option value='November'>November</option>
+                        <option value='December'>December</option>
+                    </FormControl>
                 </InputGroup>
-                <InputGroup>
-                    <FormControl
-                        placeholder="Day"
-                        aria-label="Day"
-                        aria-describedby="basic-addon1"
-                    />
+                <InputGroup
+                    key={month}
+                >
+                <FormControl
+                        as="select"
+                        disabled={month === null }
+                        onChange={(event) => {setDay(event.target.value)}}
+                    >
+                        {dayOptions}
+                        {month !== 'February' &&
+                            [<option value='29'>29</option>,
+                            <option value='30'>30</option>
+                            ]
+                        }
+                        {!(month === 'September' || month === 'April' || month === 'June' || month === 'November') && 
+                        (month !== 'February') &&
+                        <option value='31'>31</option>}
+                    </FormControl>
                 </InputGroup>
             </Form>
         </Navbar>
