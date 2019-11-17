f188ae49518b6ba94f8867c9268088d3b6a47713
omeshomes <omid.rooholfada@yale.edu>
2019-11-17 14:34:27 -0500

    added models and populate db script

--- server/models.py
+++ server/models.py
@@ -9,9 +9,26 @@ class School(db.Model):
     __tablename__ = "Schools"
     id = db.Column(UUID(as_uuid=True), primary_key=True, default=uuid.uuid4)
     name = db.Column(db.String(256))
+    weather_points = db.relationship("Weather", back_populates="school")
 
 
 class Weather(db.Model):
     __tablename__ = 'Weather'
     id = db.Column(UUID(as_uuid=True), primary_key=True, default=uuid.uuid4)
+    school_id = db.Column(UUID(as_uuid=True), db.ForeignKey("Schools.id"), index=True, nullable=False)
+    school = db.relationship("School")
+    date = db.Column(db.Date, nullable=False)
+    average_wind_speed = db.Column(db.Numeric(5, 2), nullable=True)
+    peak_gust_time = db.Column(db.Integer, nullable=True)
+    precipitation = db.Column(db.Numeric(5, 2), nullable=True)
+    max_temp = db.Column(db.Integer, nullable=True)
+    min_temp = db.Column(db.Integer, nullable=True)
+    WDF2 = db.Column(db.Integer, nullable=True)  # direction of fastest 2-minute wind
+    WDF5 = db.Column(db.Integer, nullable=True)  # direction of fastest 5-minute wind
+    WSF2 = db.Column(db.Numeric(5, 2), nullable=True)  # fastest 2-minute wind speed
+    WSF5 = db.Column(db.Numeric(5, 2), nullable=True)  # fastest 5-minute wind speed
+    has_fog = db.Column(db.Boolean, nullable=True)  #01
+    has_heavy_fog = db.Column(db.Boolean, nullable=True)  #02
+    has_thunder = db.Column(db.Boolean, nullable=True)  #03
+    has_smoke = db.Column(db.Boolean, nullable=True)  #08
 
---
+++ server/populate_db.py
@@ -0,0 +1,41 @@
+import csv
+from datetime import datetime
+
+import server.models as models
+from server.app import db
+
+
+def add_school_weather(school_name, file_path):
+    school = models.School(name=school_name)
+    db.session.add(school)
+    db.session.commit()
+
+    reader = csv.DictReader(open(file_path))
+
+    for row in reader:
+        weather_point = models.Weather(
+            school_id=school.id,
+            average_wind_speed=float(row["AWND"]) if row["AWND"] else None,
+            peak_gust_time=int(row["PGTM"]) if row["PGTM"] else None,
+            precipitation=float(row["PRCP"]) if row["PRCP"] else None,
+            max_temp=int(row["TMAX"]) if row["TMAX"] else None,
+            min_temp=int(row["TMIN"]) if row["TMIN"] else None,
+            date=datetime.strptime(row["DATE"], "%Y-%m-%d"),
+            WDF2=int(row["WDF2"]) if row["WDF2"] else None,
+            WDF5=int(row["WDF5"]) if row["WDF5"] else None,
+            WSF2=float(row["WSF2"]) if row["WSF2"] else None,
+            WSF5=float(row["WSF5"]) if row["WSF5"] else None,
+            has_fog = True if row["WT01"] else False,
+            has_heavy_fog = True if row["WT02"] else False,
+            has_thunder = True if row["WT03"] else False,
+            has_smoke = True if row["WT08"] else False,
+        )
+        db.session.add(weather_point)
+
+    print('HERE')
+    db.session.commit()
+
+
+if __name__ == "__main__":
+    add_school_weather("Yale", "server/data/yale.csv")
+    add_school_weather("Harvard", "server/data/harvard.csv")
