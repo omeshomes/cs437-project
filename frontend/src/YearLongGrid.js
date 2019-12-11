910bc982698b30bea5fab2d4603a01db8397d6bb
evanblasy <evanblasy@gmail.com>
2019-12-10 22:58:16 -0500

    uninstalled victory from parent directory

--- frontend/src/YearLongGrid.js
+++ frontend/src/YearLongGrid.js
@@ -1,21 +1,10 @@
-import React, { Component } from 'react'
+import React from 'react'
 import ReactDOM from 'react-dom';
 import * as V from 'victory';
 import { VictoryBar } from 'victory';
 import { VictoryChart } from 'victory';
 import { VictoryArea, VictoryTheme, VictoryAxis, VictoryLine } from 'victory';
 
-
-const data = [
-  {quarter: 1, earnings: 13000},
-  {quarter: 2, earnings: 16500},
-  {quarter: 3, earnings: 14250},
-  {quarter: 4, earnings: 19000}
-];
-
-const sampleData = [
-  {quarter: 1, earnings: 20},
-];
 
 function YearLongGrid(props) {
 	const {winnerData} = props;
